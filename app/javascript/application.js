18a465709685c758885e36d0e6fe9e755d11d003
bigmosi <kinyeramo@gmail.com>
2022-10-18 23:37:24 +0300

    rails react application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import setupStore from './store';
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={setupStore()}>
+    <App />
+  </Provider>,
+);
